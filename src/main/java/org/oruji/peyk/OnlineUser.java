21a67b4d44d3bf689192fdfb0b6aab6285d06736
Amin Oruji <amin@oruji.org>
2015-02-21 20:50:01 +0330

    remove local ip from chat list

--- src/main/java/org/oruji/peyk/OnlineUser.java
+++ src/main/java/org/oruji/peyk/OnlineUser.java
@@ -1,5 +1,11 @@
 package org.oruji.peyk;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Vector;
 
 public class OnlineUser implements Runnable {
@@ -17,23 +23,45 @@ public class OnlineUser implements Runnable {
 	}
 
 	public void run() {
-//		while (true) {
+		// while (true) {
+		try {
 			userSet = new Vector<PeykUser>();
 			for (int i = 1; i <= 255; i++) {
 				PeykUser user = new PeykUser("192.168.1." + i, port);
+				if (user.getHost().equals(getMyIp()))
+					continue;
 				if (user.isOnline()) {
 					userSet.add(user);
 				}
 			}
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
 		}
-//	}
+	}
+
+	// }
 
 	public void start() {
 		new Thread(this).start();
+	}
+
+	public String getMyIp() throws SocketException {
+		String myIp = null;
+		for (Enumeration<NetworkInterface> en = NetworkInterface
+				.getNetworkInterfaces(); en.hasMoreElements();) {
+			NetworkInterface netInterface = en.nextElement();
+			if (netInterface.getName().equals("wlan0")) {
+				for (InetAddress address : Collections.list(netInterface
+						.getInetAddresses())) {
+					if (address instanceof Inet4Address)
+						myIp = address.toString().substring(1);
+				}
+			}
+		}
+		return myIp;
 	}
 }
