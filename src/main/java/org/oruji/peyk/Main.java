2f3c6e27ebbc862fb8fb86732c6fafa1c932668e
Amin Oruji <amin@oruji.org>
2015-02-26 20:33:09 +0330

    add UDF communication

--- src/main/java/org/oruji/peyk/Main.java
+++ src/main/java/org/oruji/peyk/Main.java
@@ -1,11 +1,18 @@
 package org.oruji.peyk;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 	public static void main(String[] args) {
-
 		final int port = 8180;
 
+		Set<PeykUser> peykUsers = new HashSet<PeykUser>();
+
 		new GreetingServer(port).start();
+		new OnlineListenUdp(port, peykUsers).start();
+
+		new OnlineBroadCast(port).start();
 
 		new PeykFrame(port);
 	}
